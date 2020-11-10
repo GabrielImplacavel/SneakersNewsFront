e9860d5a4520037fea07403a83804e15b398d928
Gabriel Juliano <gabriel.oliveira8@hotmail.com>
2020-11-10 16:32:31 -0300

    Estudo sobre Props dos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -5,45 +5,48 @@ import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import adidas from './img/adidas.jpg';
+import jordan from './img/jordan.jpg';
+import nike from './img/nike.jpg';
 
-
-function Botao(){
-  return <Button variant="outline-dark">Dark</Button>
-}
 
 interface MyProps{
-  image?:string;
-  title?:string;
+  image:string;
+  title:string;
+  text:string;
+  button:string;
 };
 
 
-function Cartao({image}:MyProps){
+function Cartao({image,title,text,button}:MyProps){
   return (
   <Card style={{ width: '18rem' }}>
     <Card.Img variant="top" src={image}/>
     <Card.Body>
-    <Card.Title><strong>Jordan 720 Aerospace</strong></Card.Title>
-    <Card.Text>
-      Um tênis fodido de bonito mas não se encontra
-      mais nas lojas brasileiras
-    </Card.Text>
-    <Button variant="primary">Ver modelo</Button>
+  <Card.Title >{title}</Card.Title>
+    <Card.Text>{text}</Card.Text>
+  <Button variant="primary">{button}</Button>
   </Card.Body>
 </Card>)
 }
 
 function Title(){
-  return <h1>Bem vindo a sneakers news</h1>
+  return <h1>Bem vindo a o Sneaker Library</h1>
 }
 
 function Layout(){
   return (
   <Container>
-  <Row><Title/></Row>
+  <Row className="text-center">
+    <Col>
+      <Title/>
+    </Col>
+  </Row>
+
   <Row>
-  <Col><Cartao image="https://magicfeet.vteximg.com.br/arquivos/ids/177878-500-500/Tenis-Air-Jordan-1-Mid-GS-Infantil-Multicolor.jpg?v=637232446316130000" /></Col>
-  <Col><Cartao image="https://images.lojanike.com.br/1024x1024/produto/261404_2611462_20201007114540.jpg"/></Col>
-  <Col></Col>
+  <Col><Cartao button="Visualizar" title="Jordan" text="Tênis exclusivamente pensados e inspirados para basquete e StreetWear" image={jordan} /></Col>
+  <Col><Cartao title="Adidas Originals" button="Visualizar" image={adidas} text="As famosas três listras (Sessão Originals)"/></Col>
+  <Col><Cartao title="Nike" button="Visualizar" image={nike} text="O famoso swoosh"/></Col>
   </Row>
   <Row>
   <Col></Col>
@@ -59,8 +62,6 @@ function Layout(){
 function App() {
   return (
     <div className="App">
-      <Title />
-       <Botao />
        <Layout />
     </div>
   );
